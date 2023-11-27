4794cbaefdaec179b1b8d77119146a1ea0ef4125
os-sulX1 <sultanxdpx@gmail.com>
2023-11-27 22:39:54 +0300

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import {Button} from '@radix-ui/themes'
 
 type Props = {};
 
 const IssuesPage = (props: Props) => {
-  return <div className="text-black">IssuesPage</div>;
+  return (
+    <div className="text-black">
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
