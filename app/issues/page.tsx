394b421e16c4e4c2fe2dfc8c91ec91e6e5f1a8c9
os-sulX1 <sultanxdpx@gmail.com>
2023-11-29 22:23:50 +0300

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import {Button} from '@radix-ui/themes'
 
+import React from "react";
+import { Button } from '@radix-ui/themes'
+import Link from "next/link";
+//48:15
 type Props = {};
 
 const IssuesPage = (props: Props) => {
   return (
     <div className="text-black">
-      <Button>New Issue</Button>
+      <Button><Link href='../issues/new' >New Issue</Link> </Button>
     </div>
   );
 };
